fdfb86229dcb984100c5f729d6f5222f4128a0f8
Dandresfs <dandresfsoto@gmail.com>
2017-04-21 16:36:46 -0500

    Test realtime V6

--- Door/settings/base.py
+++ Door/settings/base.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = '/'
 
 CHANNEL_LAYERS = {
     "default": {
--- users/models.py
+++ users/models.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from Door.settings.base import STATIC_URL
 
 class UserManager(BaseUserManager):
     def _create_user(self, email, password, **extra_fields):
@@ -55,6 +56,10 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     def get_full_name(self):
         return self.email
+
+    def get_photo(self):
+        avatar = STATIC_URL+"img/andes_user.jpg"
+        return avatar
 
     def __unicode__(self):
         return self.first_name + ' ' + self.last_name + ' - ' + self.email
